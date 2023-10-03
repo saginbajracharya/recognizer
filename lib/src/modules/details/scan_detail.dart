7853fbbb71b908b3673261c9667eb4ef19c35113
sagin bajracharya <saginbajracharya09@gmail.com>
2023-10-03 15:40:50 +0545

    [fix]:: image scan result print

--- lib/src/modules/details/scan_detail.dart
+++ lib/src/modules/details/scan_detail.dart
@@ -21,6 +21,7 @@ class _ScanDetailState extends State<ScanDetail> {
         automaticallyImplyLeading: true,
       ),
       body: BaseLayout(
+        customMargin: const EdgeInsets.only(left:30.0,right:30.0),
         child: widget.fromPage=='liveScan'
         ? liveScanResult()
         : imageScanResult()
@@ -97,58 +98,87 @@ class _ScanDetailState extends State<ScanDetail> {
   }
 
   imageScanResult(){
-    return Column(
-      mainAxisAlignment : MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      mainAxisSize      : MainAxisSize.max,
-      children: [
-        //Cash Receipt 
-        //Date
-        //Items List
-        //Total
-        //Cash
-        //Change
-        Flexible(
-          child: ListView.builder(
-            shrinkWrap: true,
-            itemCount: recognizerCon.recognizedTextData.blocks.length,
-            itemBuilder: (context, index) {
-              final text = recognizerCon.recognizedTextData.blocks[index].text;
-              if (index == 0 || index == 1) {
-                // For index 0 (Cash Receipt), display it in a row with Date (index 1)
-                if (index == 0) {
-                  return Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(text),
-                      const SizedBox(width: 8), // Add spacing between Cash Receipt and Date
-                    ],
-                  );
-                }
-                else if(index ==1){
-                  return Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(text)
-                    ],
-                  );
-                }
-              } else {
-                return Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(text),
-                  ],
-                );
-              }
-              return null;
-            },
-          ),
-        ),
-      ],
+    List<String> textList = recognizerCon.recognizedTextData.split('\n');
+    List<String> items = [];
+    List<String> prices = [];
+
+    // Separate items and prices
+    for (int i = 0; i < textList.length; i ++) {
+      if(textList[i].contains('Date')&& i == 0)
+      {
+        //do nothing
+      }
+      else{
+        bool containsNumber = RegExp(r'\d').hasMatch(textList[i]);
+        if (containsNumber) {
+          prices.add(textList[i]);
+        } else {
+          items.add(textList[i]);
+        }
+      }
+    }
+
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisAlignment : MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize      : MainAxisSize.max,
+        children: [
+          //Cash Receipt 
+          //Date
+          //Items List
+          //Total
+          //Cash
+          //Change
+          textList[0].contains("Cash")
+          ?Column(
+            children: [
+              SelectableText(textList[0]),
+              const Divider()
+            ],
+          )
+          :const SizedBox(),
+          Row(
+            children: [
+              Expanded(
+                flex: 1, // This will make the first list take 50% of the available space
+                child: SizedBox(
+                  height: MediaQuery.of(context).size.height,
+                  child: ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: items.length,
+                    itemBuilder: (context, itemsIndex) {
+                      return SelectableText(items[itemsIndex],textAlign: TextAlign.start);
+                    },
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 1, // This will make the second list take 50% of the available space
+                child: SizedBox(
+                  height: MediaQuery.of(context).size.height,
+                  child: ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: prices.length,
+                    itemBuilder: (context, pricesIndex) {
+                      return SelectableText(prices[pricesIndex],textAlign: TextAlign.end);
+                    },
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
     );
+  }
+
+  checkIsNumber(inputString)async{
+    bool containsNumber = RegExp(r'\d').hasMatch(inputString);
+    if (containsNumber) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
--- lib/src/widgets/base_layout.dart
+++ lib/src/widgets/base_layout.dart
@@ -2,12 +2,13 @@ import 'package:flutter/material.dart';
 
 class BaseLayout extends StatelessWidget {
   final Widget child;
-  const BaseLayout({super.key,required this.child});
+  final EdgeInsetsGeometry? customMargin;
+  const BaseLayout({super.key,required this.child,this.customMargin});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.all(10.0),
+      margin: customMargin ?? const EdgeInsets.all(10.0),
       alignment: Alignment.center,
       child: child,
     );
